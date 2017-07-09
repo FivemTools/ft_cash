f46bb75b83658433591d808f68e0097ca7cd2d82
Samuel Ds <samuelds.pro@gmail.com>
2017-07-09 19:40:15 +0200

    Update 0.2.1

--- cl_cash.lua
+++ cl_cash.lua
@@ -1,6 +1,6 @@
 -- @Date:   2017-07-01T12:38:38+02:00
 -- @Project: FiveM Tools
--- @Last modified time: 2017-07-09T19:29:03+02:00
+-- @Last modified time: 2017-07-09T19:38:43+02:00
 -- @License: GNU General Public License v3.0
 
 function GetCash()
@@ -11,12 +11,4 @@ RegisterNetEvent("ft_cash:ClGetCash")
 AddEventHandler("ft_cash:ClGetCash", function(callback)
   local cash = GetCash()
   callback(cash)
-end)
-
--- Dead Event
-RegisterNetEvent("baseevents:onPlayerDied")
-AddEventHandler("baseevents:onPlayerDied", function(callback)
-  if Config.removeOnDead then
-    
-  end
 end)
--- sv_events.lua
+++ sv_events.lua
@@ -1,6 +1,6 @@
 -- @Date:   2017-07-01T12:38:45+02:00
 -- @Project: FiveM Tools
--- @Last modified time: 2017-07-09T19:35:46+02:00
+-- @Last modified time: 2017-07-09T19:39:31+02:00
 -- @License: GNU General Public License v3.0
 
 RegisterServerEvent('ft_cash:SvGetCash')
@@ -26,4 +26,12 @@ end)
 RegisterServerEvent('ft_cash:SvSetCash')
 AddEventHandler('ft_cash:SvSetCash', function(receiver, mount)
   SetCash(receiver, mount)
+end)
+
+-- Dead Event
+RegisterNetEvent("baseevents:onPlayerDied")
+AddEventHandler("baseevents:onPlayerDied", function()
+  if Config.removeOnDead then
+    SetCash(source, 0)
+  end
 end)
