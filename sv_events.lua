7d299e3a273a8f1ae3ac71c7877af000a4229bdc
Samuel Ds <samuelds.pro@gmail.com>
2017-07-09 19:41:39 +0200

    Update 0.2.2

--- sv_events.lua
+++ sv_events.lua
@@ -1,6 +1,6 @@
 -- @Date:   2017-07-01T12:38:45+02:00
 -- @Project: FiveM Tools
--- @Last modified time: 2017-07-09T19:39:31+02:00
+-- @Last modified time: 2017-07-09T19:41:13+02:00
 -- @License: GNU General Public License v3.0
 
 RegisterServerEvent('ft_cash:SvGetCash')
@@ -29,7 +29,7 @@ AddEventHandler('ft_cash:SvSetCash', function(receiver, mount)
 end)
 
 -- Dead Event
-RegisterNetEvent("baseevents:onPlayerDied")
+RegisterServerEvent("baseevents:onPlayerDied")
 AddEventHandler("baseevents:onPlayerDied", function()
   if Config.removeOnDead then
     SetCash(source, 0)
